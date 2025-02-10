8d45d301ff10c67d5be1f4c1211fae446c9adfc8
Sh1nePr0 <fedoreev77@mail.ru>
2025-02-10 17:20:58 +0300

    Update: Bind build system such as premake and create .bat file to create project faster
    Create premake5.lua build file and GenerateProjects.bat to build faster

---
+++ premake5.lua
@@ -0,0 +1,109 @@
+workspace "Independent"
+	architecture "x64"
+	startproject "IndependentEditor"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Independent"
+	location "Independent"
+	kind "SharedLib"
+	language "C++"
+	buildoptions "/utf-8"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+
+		defines
+		{
+			"IDPD_PLATFORM_WINDOWS",
+			"IDPD_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ..\\bin\\" .. outputdir .. "\\IndependentEditor")
+		}
+
+	filter "configurations:Debug"
+		defines "IDPD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "IDPD_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "IDPD_DIST"
+		optimize "On"
+
+project "IndependentEditor"
+	location "IndependentEditor"
+	kind "ConsoleApp"
+	language "C++"
+	buildoptions "/utf-8"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Independent/vendor/spdlog/include",
+		"Independent/src"
+	}
+
+	links
+	{
+		"Independent"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"IDPD_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "IDPD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "IDPD_Release"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "IDPD_DIST"
+		optimize "On"
